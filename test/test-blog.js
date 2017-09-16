8bee9524898da97e6dbfd42099eee8a0b363ae4b
John Knotts <jhnnyk@gmail.com>
2017-09-16 11:03:45 -0600

    implement first test, woohoo

--- test/test-blog.js
+++ test/test-blog.js
@@ -6,3 +6,21 @@ const {app, runServer, closeServer} = require('../server')
 const should = chai.should()
 
 chai.use(chaiHttp)
+
+describe('Blog', function () {
+  before(function () {
+    return runServer()
+  })
+
+  after(function () {
+    return closeServer()
+  })
+
+  it('should list all posts on GET', function () {
+    return chai.request(app)
+      .get('/blog-post')
+      .then(function (res) {
+        res.should.have.status(200)
+      })
+  })
+})
