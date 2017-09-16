54bc52f335feef7741c33b68f7e52216422b10b6
John Knotts <jhnnyk@gmail.com>
2017-09-16 11:25:30 -0600

    add tests for POST blog-post

--- test/test-blog.js
+++ test/test-blog.js
@@ -28,8 +28,27 @@ describe('Blog', function () {
 
         res.body.forEach(function (post) {
           post.should.be.a('object')
-          post.should.include.keys(['id', 'title', 'content', 'author'])
+          post.should.include.keys('id', 'title', 'content', 'author')
         })
+      })
+  })
+
+  it('should add a post on POST', function () {
+    const newPost = {
+      title: 'Blog Post from Test',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum praesentium eaque iste facere, consectetur obcaecati consequuntur minima excepturi nihil accusamus. Laborum vero, inventore veniam esse eos tempore odio vel dolorem!',
+      author: 'Johnny Test'
+    }
+
+    return chai.request(app)
+      .post('/blog-post')
+      .send(newPost)
+      .then(function (res) {
+        res.should.have.status(201)
+        res.should.be.json
+        res.body.should.be.a('object')
+        res.body.should.include.keys('id', 'title', 'content', 'author')
+        res.body.id.should.not.be.null
       })
   })
 })
